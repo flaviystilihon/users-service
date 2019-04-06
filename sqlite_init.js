a47a8a4ceb6608c8af5d2be6c749659ba809b1ac
Vladimir <vladimiremail@gmail.com>
2019-04-06 12:12:47 +0300

    Create Users service

---
+++ sqlite_init.js
@@ -0,0 +1,11 @@
+// var sqlite3 = require('sqlite3');
+
+
+// var db;
+// const dbfile = 'users-sequelize.sqlite3';
+// db = new sqlite3.Database(dbfile,
+//     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+//     err => {
+//         console.log(err);
+//     }
+// );
